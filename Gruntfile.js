8e541dac4ca4606e9afaa969ebb4ce77b0cfa668
Tri Nguyen <tri@tridnguyen.com>
2013-10-04 02:14:01 -0400

    some cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
 					middleware: function(connect, options) {
 						return [
 							// serve files in /dist as if they were in the root.
-							connect.static(__dirname + '/build/www'),
+							connect.static(__dirname + '/<%= config.buildPath %>'),
 							// but serve everything else from the root
 							connect.static(__dirname)
 						];
@@ -227,10 +227,6 @@ module.exports = function(grunt) {
 		'build',
 		'gh-pages'
 	])
-
-	grunt.registerTask('prod', [
-
-	]);
 
 	grunt.registerTask('default', ['dev']);
 }
